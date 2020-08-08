382316315eb1fa67165b40f1c570a29ed9fa8e56
Laura Bennett <lbenn@us.ibm.com@Laura-newMBP.local>
2020-08-08 13:23:51 -0700

    uploading new video conversion code

--- backend/STT-Audio/app.js
+++ backend/STT-Audio/app.js
@@ -15,6 +15,17 @@
  */
 
 const express = require('express');
+//var ffmpeg = require('/usr/local/bin/ffmpeg');
+var FfmpegCommand = require('fluent-ffmpeg');
+var path = require('path');
+const fs = require('fs');
+var tmpdir = __dirname + '/tmpdir';
+    
+if (!fs.existsSync(tmpdir))
+      fs.mkdirSync(tmpdir);     
+           
+
+//var converted_video_file = fs.openSync(__dirname + '/' + 'tmpdir' + '/' + 'stripped_audio.mp3', 'w');  //temporary file for converting videos
 
 const app = express();
 const { IamTokenManager } = require('ibm-watson/auth');
@@ -35,7 +46,7 @@ require('dotenv').config({ silent: true });
 const stt = require('watson-speech/speech-to-text')
 
 const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
-const fs = require('fs');
+
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 
@@ -72,10 +83,32 @@ app.use(multer({dest:'./uploads/'}).any());
 
 app.post('/transcribe', (req, res) => {
   let files = req.files
+
   files.map( (file) => {
+
+    // check if file is a video - if so we need to run it through tool to convert to .mp3
+    let extension = path.extname(file.originalname);
+    let basename = path.basename(file.originalname);
+    var transcribed_file = __dirname + '/' + file.path;
+    var converted_video_file = __dirname + '/' + 'tmpdir' + '/' + 'stripped_audio.mp3'
+    
+    if (extension == ".mp4") {
+        convert(transcribed_file, converted_video_file, function(err){
+        //convert(transcribed_file, __dirname + '/' + 'tmpdir' + '/' + 'stripped_audio.mp3', function(err){
+        if(!err) {
+          console.log('conversion complete');
+          transcribed_file = converted_video_file;
+        }
+        else {
+          console.log('conversion error')
+        }
+      });
+    }
+
+    // run file through Speech to text API and send back the transcript
     const recognizeParams = {
-      audio: fs.createReadStream(__dirname + '/' + file.path),
-      contentType: 'audio/mp3',
+      audio: fs.createReadStream(transcribed_file),
+      contentType: 'application/octet-stream',
     }
     speechToText.recognize(recognizeParams)
       .then(speechRecognitionResults => {
@@ -101,11 +134,20 @@ app.get('/api/v1/credentials', async (req, res, next) => {
   }
 });
 
-// Send message to SalesForce - REST API setup
-/* app.post('/send_audio_message', (req, res) => {
-    if (!req.body) return res.sendStatus(400);
-    console.log(req.body, "body");
-    res.send('welcome, ' + req.body.username)
-});*/
+
+// Use the Ffmpeg library to convert a video to audio
+function convert(input, output, callback) {
+
+    FfmpegCommand(input)
+        .output(output)
+        .on('end', function() {                    
+            console.log('conversion ended');
+            callback(null);
+        }).on('error', function(err){
+            console.log('error: ', e.code, e.msg);
+            callback(err);
+        }).run();
+}
+
 
 module.exports = app;
