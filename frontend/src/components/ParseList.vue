d591e354314b03d94ab0436c72f4ef80c758e9a0
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-27 18:46:50 +0800

    Delete frontend directory

--- frontend/src/components/ParseList.vue
+++
@@ -1,8 +0,0 @@
-<template v-if="Object.keys(row).includes('Score') && row['Score'].includes('|')">
-   <li v-for="score in row['Score'].split('|')">
-       {{score}}
-   </li>
-</template>
-<template v-else>
-    {{row['Score']}}
-</template>
