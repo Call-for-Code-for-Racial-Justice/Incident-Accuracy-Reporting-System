17ddc727656694a271015b218343fdff796abb09
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-27 18:44:46 +0800

    Delete backend directory

--- backend/STT-Audio/public/scripts/bundle.jsx
+++
@@ -1,6 +0,0 @@
-import './polyfills';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Demo from '../../views/demo.jsx';
-
-ReactDOM.render(<Demo />, document.getElementById('root'));
