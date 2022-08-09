805601c5f02bda7cea302a1220a2848300063c79
Charles Johnson <cjohnso@us.ibm.com>
2022-08-09 14:23:47 -0500

    Adding initial OpenLiberty projects.

--- backend/STT-Audio/public/scripts/polyfills.js
+++
@@ -1,9 +0,0 @@
-// todo: serve this file seperately and only load when needed...
-
-// polyfills for Promise, Object.assign, etc.
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-// fetch polyfill for Safari / IE
-// automatically sets global
-import 'whatwg-fetch';
