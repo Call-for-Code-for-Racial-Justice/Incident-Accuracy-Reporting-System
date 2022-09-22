fbe57d929650856d621f083ab4a545d4759f1409
Charles Johnson <cjohnso@us.ibm.com>
2022-09-22 02:27:31 -0500

    Initial MVP.

---
+++ mediaupload-service/src/main/java/org/callforcode/iars/application/MediaUploadApplication.java
@@ -0,0 +1,9 @@
+package org.callforcode.iars.application;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class MediaUploadApplication extends Application {
+    
+}
